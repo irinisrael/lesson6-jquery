2c07d76bc0541b3c1b225fe38950832ebc4f4594
Irin Israel <irinisrael@icloud.com>
2015-10-19 11:12:55 -0400

    made doc.ready into one giant anonymous function for all

--- haunted_page_javascript.js
+++ haunted_page_javascript.js
@@ -1,29 +1,17 @@
 $(document).ready(function() {
 	alert('Boo!');
-	})
-
-$(document).ready(function() {
 	$('button').click(function() {
 		$('h1').hide();
 	});
-})
-
-$(document).ready(function() {
 	$('h5').click(function() {
 		$('h5').fadeOut(1000);
 	});
-})
-
-$(document).ready(function() {
 	$('img').animate( {
 		width: "10%",
 		height: "10%"
 	}, 3000);
-})
-
-$(document).ready(function() {
 	$('img').animate( {
 		width: "100%",
 		height: "100%"
 	}, 3000);
-})
+	})
